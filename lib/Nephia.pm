2d2c64f7751140bc6f7aa4e3b0981caed8713d3c
ytnobody <ytnobody@gmail.com>
2012-07-18 08:19:01 +0900

    initial commit

---
+++ lib/Nephia.pm
@@ -0,0 +1,32 @@
+package Nephia;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Nephia -
+
+=head1 SYNOPSIS
+
+  use Nephia;
+
+=head1 DESCRIPTION
+
+Nephia is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
