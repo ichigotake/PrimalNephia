2b4a962fa3adda78fbb5cceeca791b660a35cb50
ytnobody <ytnobody@gmail.com>
2013-07-02 12:21:40 +0900

    Checking in changes prior to tagging of version 0.23.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1e982e8..ac4dc60 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Nephia
    
     {{$NEXT}}
    +
    +0.23 2013-07-02T03:21:31Z
             - refactor: Split DSLModifier as external module
    
     0.22 2013-06-28T15:16:13Z

--- lib/Nephia.pm
+++ lib/Nephia.pm
@@ -2,7 +2,7 @@ package Nephia;
 use strict;
 use warnings;
 use File::Spec;
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 sub import {
     my ($class, %opts) = @_;
--- t/nephia-test_app/lib/Nephia/TestApp.pm
+++ t/nephia-test_app/lib/Nephia/TestApp.pm
@@ -4,7 +4,7 @@ use warnings;
 use Nephia plugins => qw/Bark/;
 use utf8;
 
-our $VERSION = 0.22;
+our $VERSION = 0.23;
 
 my $item = 'ひのきのぼう';
 
