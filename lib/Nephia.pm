b818d6952b8daa2299d3bd5f918303aa7e7514f5
ytnobody <ytnobody@gmail.com>
2013-07-03 15:44:08 +0900

    Checking in changes prior to tagging of version 0.25.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d0d9531..3913e4e 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Nephia
    
     {{$NEXT}}
    +
    +0.25 2013-07-03T06:43:55Z
             - feature : add cookie / set_cookie DSL
    
     0.24 2013-07-03T01:52:36Z

--- lib/Nephia.pm
+++ lib/Nephia.pm
@@ -2,7 +2,7 @@ package Nephia;
 use strict;
 use warnings;
 use File::Spec;
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
 sub import {
     my ($class, %opts) = @_;
--- t/nephia-test_app/lib/Nephia/TestApp.pm
+++ t/nephia-test_app/lib/Nephia/TestApp.pm
@@ -4,7 +4,7 @@ use warnings;
 use Nephia plugins => qw/Bark/;
 use utf8;
 
-our $VERSION = 0.24;
+our $VERSION = 0.25;
 
 my $item = 'ひのきのぼう';
 
