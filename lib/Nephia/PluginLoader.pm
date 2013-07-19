4eb30aeca1c4810ee9b256992c295bb6a05d7889
ytnobody <ytnobody@gmail.com>
2013-07-19 15:00:50 +0900

    more thin logic

--- lib/Nephia/PluginLoader.pm
+++ lib/Nephia/PluginLoader.pm
@@ -9,7 +9,7 @@ sub load {
 
     while (@plugins) {
         my $plugin = _normalize_plugin_name(shift(@plugins));
-        my $opt    = $plugins[0] && ref $plugins[0] ? shift @plugins : undef;
+        my $opt    = $plugins[0] && ref $plugins[0] ? shift(@plugins) : undef;
         _export_plugin_functions($plugin, $caller, $opt);
     }
 };
@@ -26,38 +26,32 @@ sub _export_plugin_functions {
     {
         no strict qw/refs subs/;
         no warnings qw/redefine prototype/;
+
         *{$plugin.'::context'} = *Nephia::Core::context;
-        $plugin->import if $plugin->can('import');
+        $plugin->import           if $plugin->can('import');
         $plugin->load($pkg, $opt) if $plugin->can('load');
 
-        for my $func ( @{"${plugin}::EXPORT"} ){
-            *{"$pkg\::$func"} = $plugin->can($func);
-        }
-        _load_hook_point($plugin);
+        *{"$pkg\::$_"} = $plugin->can($_) for @{"${plugin}::EXPORT"};
     }
+    _load_hook_point($plugin);
 }
 
 sub _load_hook_point {
-    my ($plugin) = @_;
-    no strict qw/refs/;
-    no warnings qw/redefine/;
-    if (my $plugin_action = $plugin->can('before_action')) {
-        my $orig = Nephia::Core->can('before_action');
-        *Nephia::Core::before_action = sub {
-            my ($env, $path_param, $action) = @_;
-            $plugin_action->($env, $path_param, $orig, $action);
-        };
-    }
-    for my $func (qw/process_env process_response process_content/) {
-        my $plugin_func = $plugin->can($func);
-        if ($plugin_func) {
-            my $orig = Nephia::Core->can($func);
-            my $sub = sub {
-                my $in = shift;
-                my $out = $plugin_func->($orig->($in));
-                return $out;
-            }; 
-            *{'Nephia::Core::'.$func} = $sub;
+    my $plugin = shift;
+    {
+        no strict qw/refs/;
+        no warnings qw/redefine/;
+        if (my $plugin_action = $plugin->can('before_action')) {
+            my $orig = Nephia::Core->can('before_action');
+            *Nephia::Core::before_action = sub {
+                my ($env, $path_param, $action) = @_;
+                $plugin_action->($env, $path_param, $orig, $action);
+            };
+        }
+        for my $func (qw/process_env process_response process_content/) {
+            my $plugin_func           = $plugin->can($func) or next;
+            my $orig                  = Nephia::Core->can($func);
+            *{'Nephia::Core::'.$func} = sub { $plugin_func->($orig->(shift)) }; 
         }
     }
 }
